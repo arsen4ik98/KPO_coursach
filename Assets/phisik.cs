3843807d81f5a168c74630e178e3c6a3b2b58d19
arsen4ik98 <arsenbeglaryan98@gmail.com>
2017-09-25 21:19:55 +0300

    закинул проект

---
+++ Assets/phisik.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class phisik : MonoBehaviour
+{
+
+    private float speed = 5.0f;
+    private float jump = 25.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector2(0.0f, jump * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector2(speed * Time.deltaTime, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector2(-speed * Time.deltaTime, 0.0f));
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "button")
+        {
+            
+            
+            Destroy(col.gameObject);
+        }
+    }
+}
