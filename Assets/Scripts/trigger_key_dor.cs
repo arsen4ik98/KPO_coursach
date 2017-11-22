82db867e081fc67015205eb24f2c39410425abb3
AndreySIS <32203023+AndreySIS@users.noreply.github.com>
2017-11-22 12:55:08 +0300

    анимация кнопки
    Арсен, делай новые уровни

--- Assets/Scripts/trigger_key_dor.cs
+++ Assets/Scripts/trigger_key_dor.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 
 public class trigger_key_dor : MonoBehaviour {
-    public Animator animator1;
+    public Animator animator;
     new private Rigidbody2D rigidbody ;
     public GameObject door , key_open, key_close, gras;
+    public Transform ClickCheck;
 
 	// Use this for initialization
 	void Start () {
@@ -14,27 +15,31 @@ public class trigger_key_dor : MonoBehaviour {
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        animator1 = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 	// Update is called once per frame
 	void Update () {
 	    
 	}
-   public void OnTriggerEnter2D(Collider2D col)
+
+    void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "button")
         {
-            Debug.Log("111");
+            animator.SetBool("Click", true);
             door.transform.position = new Vector2(-0.06f, -3.88f);
-            Destroy(key_open);
-            animator1.Play("open");
-            Debug.Log("animation");
         }
+        //if (col.gameObject.tag != "button")
+        //{
+        //    animator.SetBool("Click", false);
+        //}
+    }
+
+   public void OnTriggerEnter2D(Collider2D col)
+    {
         if (col.gameObject.tag == "button1")
         {
             gras.transform.localScale = new Vector2(1.6f, 1f);
-           // door.PlayAnimation(animation.Play("New Animation"));
-           // AnimationState time = animator(hgv);
         }
 
     }
